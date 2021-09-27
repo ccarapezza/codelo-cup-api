57fe6d3595b3850a2a13dbbcee994c86199add51
Matias <matias.vighi@gmail.com>
2021-09-26 23:15:05 -0300

    production  database config

--- app/models/index.js
+++ app/models/index.js
@@ -1,23 +1,31 @@
 const config = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  config.DB,
-  config.USER,
-  config.PASSWORD,
-  {
-    host: config.HOST,
-    dialect: config.dialect,
-    operatorsAliases: false,
+const sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize(process.env.DATABASE_URL, {
+        host: config.HOST,
+        dialect: config.dialect,
+        operatorsAliases: false,
+        pool: {
+          max: config.pool.max,
+          min: config.pool.min,
+          acquire: config.pool.acquire,
+          idle: config.pool.idle,
+        },
+      })
+    : new Sequelize(config.DB, config.USER, config.PASSWORD, {
+        host: config.HOST,
+        dialect: config.dialect,
+        operatorsAliases: false,
 
-    pool: {
-      max: config.pool.max,
-      min: config.pool.min,
-      acquire: config.pool.acquire,
-      idle: config.pool.idle
-    }
-  }
-);
+        pool: {
+          max: config.pool.max,
+          min: config.pool.min,
+          acquire: config.pool.acquire,
+          idle: config.pool.idle,
+        },
+      });
 
 const db = {};
 
@@ -30,12 +38,12 @@ db.role = require("../models/role.model.js")(sequelize, Sequelize);
 db.role.belongsToMany(db.user, {
   through: "user_roles",
   foreignKey: "roleId",
-  otherKey: "userId"
+  otherKey: "userId",
 });
 db.user.belongsToMany(db.role, {
   through: "user_roles",
   foreignKey: "userId",
-  otherKey: "roleId"
+  otherKey: "roleId",
 });
 
 db.ROLES = ["user", "admin", "moderator"];
