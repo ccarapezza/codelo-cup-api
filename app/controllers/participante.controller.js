166fade57347147dc9c3371145b00e2806a09b7d
ccarapezza <carapezza.christian@gmail.com>
2021-10-03 20:36:57 -0300

    Add create participante functions

--- app/controllers/participante.controller.js
+++ app/controllers/participante.controller.js
@@ -4,20 +4,20 @@ const Muestra = db.muestra;
 
 const Op = db.Sequelize.Op;
 
-var bcrypt = require("bcryptjs");
+const crypto = require('crypto');
 
 exports.create = (req, res) => {
   const data = req.body;
   //
   Participante.create({
     name: data.name,
-    hash: bcrypt.hashSync(new Date().getTime().toString(), 10),
+    hash: crypto.createHash('sha1').update(new Date().getTime().toString()).digest('hex')
   })
     .then((participante) => {
       const hashedMuestras = data.muestras.map((muestra) => {
         return {
           ...muestra,
-          hash: bcrypt.hashSync(new Date().getTime().toString(), 10),
+          hash: crypto.createHash('sha1').update(new Date().getTime().toString()).digest('hex')
         };
       });
       Muestra.bulkCreate(hashedMuestras).then((muestras) => {
@@ -33,48 +33,33 @@ exports.create = (req, res) => {
     });
 };
 
-exports.signin = (req, res) => {
-  User.findOne({
-    where: {
-      username: req.body.username,
-    },
+exports.findAll = (req, res) => {
+  Participante.findAll({
+    include: [
+      {model: Muestra, as: Muestra.tableName}
+    ]
   })
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
+  .then((participantes) => {
+    res.status(200).send(participantes);
+  })
+  .catch((err) => {
+    res.status(500).send({ message: err.message });
+  });
+};
 
-      var passwordIsValid = bcrypt.compareSync(
-        req.body.password,
-        user.password
-      );
-
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!",
-        });
-      }
-
-      var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400, // 24 hours
-      });
-
-      var authorities = [];
-      user.getRoles().then((roles) => {
-        for (let i = 0; i < roles.length; i++) {
-          authorities.push("ROLE_" + roles[i].name.toUpperCase());
-        }
-        res.status(200).send({
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          roles: authorities,
-          accessToken: token,
-        });
-      });
-    })
-    .catch((err) => {
-      res.status(500).send({ message: err.message });
-    });
+exports.participanteLogin = (req, res) => {
+  const hash = req.body.hash;
+  Participante.findOne({
+    where: {
+      hash: hash,
+    }
+  }).then((participante) => {
+    if(participante){
+      res.status(200).send(participante);
+    }else{
+      res.status(500).send({ message: "Datos inválidos." });  
+    }
+  }).catch((err) => {
+    res.status(500).send({ message: err.message });
+  });
 };
--- app/routes/participante.routes.js
+++ app/routes/participante.routes.js
@@ -1,5 +1,6 @@
 const { verifySignUp } = require("../middleware");
 const controller = require("../controllers/participante.controller");
+const { check, validationResult } = require('express-validator');
 
 module.exports = function (app) {
   app.use(function (req, res, next) {
@@ -10,5 +11,31 @@ module.exports = function (app) {
     next();
   });
 
-  app.post("/api/participante/create", controller.create);
+  app.post(
+    "/api/participante/create",
+    [
+      check('name').exists({checkFalsy: true}),
+      check('muestras').isArray().notEmpty().custom((value, { req }) => {
+        const muestras = req.body.muestras;
+        for (const muestra of muestras) {
+          if(!muestra.name){
+            throw new Error('El nombre de las muestras es obligatorio');
+          }
+        }
+        return true;
+      }),
+      (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+      }
+    ],
+    controller.create
+  );
+
+  app.get("/api/participante/list", controller.findAll);
+
+  app.post("/api/participante/login", controller.participanteLogin);
 };
