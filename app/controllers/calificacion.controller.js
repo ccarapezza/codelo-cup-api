a8b69be65da90dc19c885afb586c1f1d8e1090b0
ccarapezza <carapezza.christian@gmail.com>
2021-10-05 21:01:11 -0300

    calificaciones

--- app/controllers/calificacion.controller.js
+++ app/controllers/calificacion.controller.js
@@ -7,32 +7,46 @@ const Op = db.Sequelize.Op;
 
 const crypto = require('crypto');
 
-exports.calificar = (req, res) => {
+exports.validar = (req, res) => {
   const data = req.body;
+  const participante = req.participante
 
   Muestra.findOne({
     where: {
-      hash: hash,
+      hash: data.hash,
     }
   }).then((muestra) => {
-    
+    res.status(200).send({ id: muestra.id });
   })
   .catch((err) => {
     res.status(500).send({ message: err.message });
   });
+};
 
-  //
-  Calificacion.create({
-    muestra: muestra,
-    participante: muestra,
-    presentacion: "",
-    aromaPrendido: "",
-    aromaApagado: "",
-    saborPrendido: "",
-    saborApagado: ""
-  })
-  .then((calificacion) => {
-    
+exports.calificar = (req, res) => {
+  const data = req.body;
+  const participante = req.participante
+
+  Muestra.findOne({
+    where: {
+      hash: data.hash,
+    }
+  }).then((muestra) => {
+    Calificacion.create({
+      muestra: muestra,
+      participante: participante,
+      presentacion: data.participante,
+      aromaPrendido: data.aromaPrendido,
+      aromaApagado: data.aromaApagado,
+      saborPrendido: data.saborPrendido,
+      saborApagado: data.saborApagado
+    })
+    .then((calificacion) => {
+      res.status(200).send({ calificacion: calificacion });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
   })
   .catch((err) => {
     res.status(500).send({ message: err.message });
--- app/routes/calificacion.routes.js
+++ app/routes/calificacion.routes.js
@@ -20,6 +20,26 @@ module.exports = function (app) {
     );
     next();
   });
+
+  app.post(
+    "/api/participante/validar-muestra",
+    [
+      check('hashMuestra').exists({checkFalsy: true}).custom((value, { req }) => {return !isNaN(value);}),
+      check('presentacion').exists({checkFalsy: true}).custom((value, { req }) => {return validateCalificacion(value);}),
+      check('aromaPrendido').exists({checkFalsy: true}).custom((value, { req }) => {return validateCalificacion(value);}),
+      check('aromaApagado').exists({checkFalsy: true}).custom((value, { req }) => {return validateCalificacion(value);}),
+      check('saborPrendido').exists({checkFalsy: true}).custom((value, { req }) => {return validateCalificacion(value);}),
+      check('saborApagado').exists({checkFalsy: true}).custom((value, { req }) => {return validateCalificacion(value);}),
+      (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+      }
+    ],
+    controller.validar
+  );
   
   app.post(
     "/api/participante/calificar",
