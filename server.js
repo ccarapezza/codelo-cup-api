fea3729968dc64cfac801792d396b815ef625bff
ccarapezza <carapezza.christian@gmail.com>
2023-01-02 19:00:08 -0300

    Clean dirty code

--- server.js
+++ server.js
@@ -167,11 +167,6 @@ require("./app/routes/dojo.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-/*
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-*/
 
 var httpServer = http.createServer(app);
 httpServer.listen(PORT, () => {
