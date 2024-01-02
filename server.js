71c150b36350ca1d767eb02b32c6dc486b05f453
ccarapezza <carapezza.christian@gmail.com>
2024-01-02 03:49:15 -0300

    stable commit

--- server.js
+++ server.js
@@ -36,15 +36,12 @@ const Categoria = db.categoria;
 const Role = db.role;
 const User = db.user;
 
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and Resync Database with { force: true }");
-  initial();
-});
+db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
   res.json({
-    message: "Bienvenido CodeloCup API. MAIN-RESYNC (" + process.env.NODE_ENV + ")",
+    message: "Bienvenido CodeloCup API. MAIN-STABLE (" + process.env.NODE_ENV + ")",
   });
 });
 
@@ -53,52 +50,6 @@ app.get("/api/is-alive", (req, res) => {
     message: "CodeloCup API (" + process.env.NODE_ENV + "), "+new Date().toDateString(),
   });
 });
-
-app.get("/api/create-random-calificaciones", async (req, res) => {
-  
-    const muestras = await Muestra.findAll({include: [Categoria]});
-    const participantes = await Participante.findAll();
-  
-    const data = [];
-
-    function getRandomNumber(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    for (const muestra of muestras) {
-      const countOfCalificacionesPerMuestra = getRandomNumber(8, 10);
-      let participantesTemp = [...participantes];
-      for (let i = 0; i < countOfCalificacionesPerMuestra; i++) {
-        let indexSel;
-        do{
-          indexSel = Math.round(Math.random()*participantesTemp.length);
-        }while(participantesTemp[indexSel]?.id===muestra.participanteId||participantesTemp[indexSel]===undefined);
-        const participante = participantesTemp.splice(indexSel, 1)[0];
-        const valores = [];
-        for (let k = 0; k < muestra.categoria.labels.split(",").length; k++) {
-          valores.push(Math.floor(Math.random() * 10) + 1);
-        }
-        data.push({
-          muestraId: muestra.id,
-          participanteId: participante.id,
-          valores: valores.toString()
-        })
-      }
-
-    }
-  
-    Calificacion.bulkCreate(data)
-    .then(() => {
-      res.json({
-        message: "Calificaciones creadas correctamente"
-      });
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || "Error al crear calificaciones"
-      });
-    });
-})
 
 app.get("/api/data", async (req, res) => {
 
@@ -167,66 +118,8 @@ require("./app/routes/dojo.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-/*
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-*/
 
 var httpServer = http.createServer(app);
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
-
-if(process.env.NODE_ENV !== "production"){
-  var httpsServer = https.createServer(credentials, app);
-  httpsServer.listen(8443, () => {
-    console.log(`Server is running on port 8443.`);
-  });
-}
-
-const initial = async () => {
-  await Role.create({id: 1, name: "user"});
-  await Role.create({id: 2, name: "moderator"});
-  await Role.create({id: 3, name: "admin"});
-
-  let user = await User.create({
-    username: "admin",
-    email: "admin@admin.com",
-    password: "$2a$08$6e/QNEys..r1DPhtHqxVvOtMAfYOg.60p6wW8VANtapcyZg652aRS", //admin
-    /*
-    password: "$2a$08$ANDS1Yo6EQSQfzHQoybU2eBCR.3Ut6t4AL099R8hI3J.NE.o4vEaW", //23737nefasta
-    password: "$2a$08$r7xBr0LQtrwkFjm27mNyountfloLujhhNF/6Adzl./VecMGUi0gVu", //c0p43d3n
-    password: "$2a$08$7ceHWSMUYjCJbW8Aal8BVuTLqKn8LBjwWgKlV0tpx5S6DzeBLzmqC", //QKfbt4fLAT
-    password: "$2a$08$6e/QNEys..r1DPhtHqxVvOtMAfYOg.60p6wW8VANtapcyZg652aRS", //admin
-    */
-  });
-
-  await user.setRoles([1]);  
-  await executeImport(db.sequelize);
-  await SystemParams.getInstance().setParam(SystemParamsKeys.RESTRICTED_BY_MESA, "false");
-}
-
-const executeImport = async (sqlz) => {
-  try {
-    var sqlString = fs.readFileSync('app/import/import.sql').toString().trim();
-    var sqlWithoutComments = sqlString.replace(/(\/\*[^*]*\*\/)|(\/\/[^*]*)|(--[^.].*)/gm, '');
-    sqlWithoutComments = sqlWithoutComments.replace(/\r?\n|\r/g, "")
-    sqlWithoutComments = sqlWithoutComments.replace(/^\s+/gm, "")
-    arr = sqlWithoutComments.split(";");
-
-    // remove empty statements
-    arr = arr.filter((el) => el != '');
-    arr = arr.map((el) => el + ';');
-
-    console.log("Array...", arr);
-
-    console.log("Executing test data...");
-    for (const sql of arr) {
-      await sqlz.query(sql);
-    }
-    console.log("Executed");
-  } catch (error) {
-    console.error('could not rebuild the views', error);
-  }
-}
